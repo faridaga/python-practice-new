b6b8f9df7a4a94caaaa155527a928a3effa9fdb6
faridaga <89481383+faridaga@users.noreply.github.com>
2023-08-03 21:38:34 +0330

    adddivfloorpow-project

---
+++ video20(sun-div-pow)/main.py
@@ -0,0 +1,8 @@
+a = 10
+b = 7
+print("div is %f"%  (a/b))
+print("floor is %f"%  (a//b))
+print("pow is %f"%  (a**b))
+
+# ok program ready now add to github this program
+# اینجا نشان میدهد کدام فایل عوض شده با زدن + میگیم کدام تغییرات اضافه شود
