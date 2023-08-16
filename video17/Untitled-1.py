ff7823868edd9392537cce2fdbd37e05071ae866
faridaga <89481383+faridaga@users.noreply.github.com>
2023-08-16 17:05:49 +0330

    add

---
+++ video17/Untitled-1.py
@@ -0,0 +1,9 @@
+print("hello world")  #string
+print("hello 'r' world")  #string show'
+print("""hello 
+      world""")  #string show in othe line triple
+print(1+2+3) #math works
+print(1000,000,000) #logic eror show100
+print(100_000_000) #show big numbers
+print(type(1)) #show type
+print(type(True))
